5e3b64f01fa2a06159ed9db9236579e2d60287a0
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-01 20:36:21 +0100

    Create tempalte

---
+++ template/src/bin/part1.rs
@@ -0,0 +1,20 @@
+fn process(input: &str) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("Output: {}", process(include_str!("./input.txt")));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = "";
+    const ANSWER: &str = "";
+
+    #[test]
+    fn example() {
+        assert_eq!(ANSWER, process(EXAMPLE))
+    }
+}
