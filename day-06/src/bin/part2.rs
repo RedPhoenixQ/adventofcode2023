3ebe5c17684303e11562a5929fbef20627a86bba
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-07 00:22:07 +0100

    part2 solved with binary search
    The binary search finds the limits were the boat no longer beats the
    distance record. The algorithm is iterative and is simply copied, changing the predicate for choosing side, for the min and max limits

--- day-06/src/bin/part2.rs
+++ day-06/src/bin/part2.rs
@@ -1,6 +1,6 @@
 use nom::{
     bytes::complete::tag,
-    character::complete::{i32, multispace1},
+    character::complete::{i64, multispace1},
     combinator::map,
     sequence::{preceded, separated_pair},
     IResult,
@@ -8,24 +8,86 @@ use nom::{
 
 #[derive(Debug)]
 struct Race {
-    time: i32,
-    distance: i32,
+    time: i64,
+    distance: i64,
 }
 
 fn process(input: &str) -> String {
     let race = parse(input);
     dbg!(&race);
 
-    todo!()
+    let mut left = 0..race.time / 2;
+    let mut right = race.time / 2..race.time;
+    while left.start != right.start {
+        dbg!(&left, &right);
+
+        let hold_time = right.start; // middle value
+        let boat_speed = hold_time;
+        let time_left_to_move = race.time - hold_time;
+        let distance_traveled = boat_speed * time_left_to_move;
+
+        let new_range = if distance_traveled > race.distance {
+            // winning at this hold_time
+            left
+        } else {
+            // not winning with this hold_time
+            right
+        };
+        dbg!(
+            distance_traveled,
+            race.distance - distance_traveled,
+            &new_range
+        );
+        let middle = new_range.start + (new_range.end - new_range.start) / 2;
+        left = new_range.start..middle;
+        right = middle..new_range.end;
+    }
+    dbg!(&left, &right);
+
+    let min = right.end;
+
+    let mut left = 0..race.time / 2;
+    let mut right = race.time / 2..race.time;
+    while left.start != right.start {
+        dbg!(&left, &right);
+
+        let hold_time = right.start; // middle value
+        let boat_speed = hold_time;
+        let time_left_to_move = race.time - hold_time;
+        let distance_traveled = boat_speed * time_left_to_move;
+
+        let new_range = if distance_traveled < race.distance {
+            // winning at this hold_time
+            left
+        } else {
+            // not winning with this hold_time
+            right
+        };
+        dbg!(
+            distance_traveled,
+            race.distance - distance_traveled,
+            &new_range
+        );
+        let middle = new_range.start + (new_range.end - new_range.start) / 2;
+        left = new_range.start..middle;
+        right = middle..new_range.end;
+    }
+    dbg!(&left, &right);
+
+    let max = left.end;
+
+    dbg!(min, max);
+
+    format!("{}", (min..=max).count())
 }
 
 fn parse(input: &str) -> Race {
     let input = input.replace(" ", "");
     let res: IResult<&str, Race> = map(
         separated_pair(
-            preceded(tag("Time:"), i32),
+            preceded(tag("Time:"), i64),
             multispace1,
-            preceded(tag("Distance:"), i32),
+            preceded(tag("Distance:"), i64),
         ),
         |(time, distance)| Race { time, distance },
     )(input.as_str());
