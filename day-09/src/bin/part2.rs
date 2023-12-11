e676f8ee7120d6e2b0ac6fd028d1935335a6fcf5
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-11 22:40:10 +0100

    part2 solved

--- day-09/src/bin/part2.rs
+++ day-09/src/bin/part2.rs
@@ -23,13 +23,13 @@ fn process(input: &str) -> String {
             })
             .collect_vec();
         dbg!(&list, &differences, only_zeros);
-        let last = *list.last().expect("to have elements in difference array");
+        let first = *list.first().expect("to have elements in difference array");
         if !only_zeros {
             let next = find_next_in_sequence(differences);
-            dbg!(last, next);
-            last + next
+            dbg!(first, next);
+            first - next
         } else {
-            last
+            first
         }
     }
 
@@ -52,7 +52,7 @@ mod test {
     const EXAMPLE: &str = "0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45";
-    const ANSWER: &str = "114";
+    const ANSWER: &str = "2";
 
     #[test]
     fn example() {
