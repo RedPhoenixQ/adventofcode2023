89ef9dbf7f0be73a28b0ae57ae6c84a9f54fb63e
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-11 22:38:17 +0100

    day-09 part1 solved

---
+++ day-09/src/bin/part1.rs
@@ -0,0 +1,61 @@
+use itertools::Itertools;
+
+fn process(input: &str) -> String {
+    let histories: Vec<Vec<i32>> = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|n| n.parse().expect("input to be number"))
+                .collect()
+        })
+        .collect();
+
+    fn find_next_in_sequence(list: Vec<i32>) -> i32 {
+        let mut only_zeros = true;
+        let differences = list
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| b - a)
+            .inspect(|n| {
+                if n != &0 {
+                    only_zeros = false;
+                }
+            })
+            .collect_vec();
+        dbg!(&list, &differences, only_zeros);
+        let last = *list.last().expect("to have elements in difference array");
+        if !only_zeros {
+            let next = find_next_in_sequence(differences);
+            dbg!(last, next);
+            last + next
+        } else {
+            last
+        }
+    }
+
+    histories
+        .into_iter()
+        .map(find_next_in_sequence)
+        .inspect(|next| println!("{next}"))
+        .sum::<i32>()
+        .to_string()
+}
+
+fn main() {
+    println!("Output: {}", process(include_str!("./input.txt")));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+    const ANSWER: &str = "114";
+
+    #[test]
+    fn example() {
+        assert_eq!(ANSWER, process(EXAMPLE))
+    }
+}
