d7a47e6065928f23df5b817fd1233161a0daddcd
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-14 01:05:11 +0100

    day-11 part1 solved

---
+++ day-11/src/bin/part1.rs
@@ -0,0 +1,82 @@
+use std::collections::BTreeSet;
+
+use glam::IVec2;
+use itertools::Itertools;
+
+fn process(input: &str) -> String {
+    let galaxies: Vec<IVec2> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.char_indices()
+                .filter_map(move |(x, c)| (c == '#').then_some(IVec2::new(x as i32, y as i32)))
+        })
+        .collect();
+
+    let min_x = galaxies.iter().map(|pos| pos.x).min().unwrap();
+    let max_x = galaxies.iter().map(|pos| pos.x).max().unwrap();
+    let min_y = galaxies.iter().map(|pos| pos.y).min().unwrap();
+    let max_y = galaxies.iter().map(|pos| pos.y).max().unwrap();
+    let rows_with_galaxies: BTreeSet<i32> = galaxies.iter().map(|pos| pos.x).collect();
+    let columns_with_galaxies: BTreeSet<i32> = galaxies.iter().map(|pos| pos.y).collect();
+
+    let rows_containing_galaxies = (min_x..max_x).into_iter().collect();
+    let mut rows_to_expand = rows_with_galaxies
+        .symmetric_difference(&rows_containing_galaxies)
+        .collect_vec();
+    let columns_containing_galaxies = (min_y..max_y).into_iter().collect();
+    let mut columns_to_expand = columns_with_galaxies
+        .symmetric_difference(&columns_containing_galaxies)
+        .collect_vec();
+
+    // dbg!(&rows_to_expand, &columns_to_expand);
+    rows_to_expand.sort();
+    columns_to_expand.sort();
+
+    let expanded_galaxies = galaxies
+        .into_iter()
+        .map(|pos| {
+            let dx = rows_to_expand.partition_point(|i| i < &&pos.x);
+            let dy = columns_to_expand.partition_point(|i| i < &&pos.y);
+            pos + IVec2::new(dx as i32, dy as i32)
+        })
+        .collect_vec();
+
+    expanded_galaxies
+        .iter()
+        .enumerate()
+        .flat_map(|(step, &galaxie)| {
+            expanded_galaxies.iter().skip(step + 1).map(move |&other| {
+                let diff = (galaxie - other).abs();
+                diff.x + diff.y
+            })
+        })
+        .sum::<i32>()
+        .to_string()
+}
+
+fn main() {
+    println!("Output: {}", process(include_str!("./input.txt")));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+    const ANSWER: &str = "374";
+
+    #[test]
+    fn example() {
+        assert_eq!(ANSWER, process(EXAMPLE))
+    }
+}
