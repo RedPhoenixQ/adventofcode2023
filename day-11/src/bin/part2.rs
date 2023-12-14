b0ea6e10ffc411cfc6ec3da8e16e21c09f7ed06f
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-14 01:27:59 +0100

    part2 solved
    apply a ratio (multiplier) to the row/column expansion

--- day-11/src/bin/part2.rs
+++ day-11/src/bin/part2.rs
@@ -1,15 +1,17 @@
 use std::collections::BTreeSet;
 
-use glam::IVec2;
+use glam::I64Vec2;
 use itertools::Itertools;
 
-fn process(input: &str) -> String {
-    let galaxies: Vec<IVec2> = input
+const EXPANSION_RATIO: i64 = 1_000_000;
+
+fn process(input: &str, ratio: i64) -> String {
+    let galaxies: Vec<I64Vec2> = input
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
             line.char_indices()
-                .filter_map(move |(x, c)| (c == '#').then_some(IVec2::new(x as i32, y as i32)))
+                .filter_map(move |(x, c)| (c == '#').then_some(I64Vec2::new(x as i64, y as i64)))
         })
         .collect();
 
@@ -17,28 +19,24 @@ fn process(input: &str) -> String {
     let max_x = galaxies.iter().map(|pos| pos.x).max().unwrap();
     let min_y = galaxies.iter().map(|pos| pos.y).min().unwrap();
     let max_y = galaxies.iter().map(|pos| pos.y).max().unwrap();
-    let rows_with_galaxies: BTreeSet<i32> = galaxies.iter().map(|pos| pos.x).collect();
-    let columns_with_galaxies: BTreeSet<i32> = galaxies.iter().map(|pos| pos.y).collect();
+    let rows_with_galaxies: BTreeSet<i64> = galaxies.iter().map(|pos| pos.x).collect();
+    let columns_with_galaxies: BTreeSet<i64> = galaxies.iter().map(|pos| pos.y).collect();
 
     let rows_containing_galaxies = (min_x..max_x).into_iter().collect();
-    let mut rows_to_expand = rows_with_galaxies
+    let rows_to_expand = rows_with_galaxies
         .symmetric_difference(&rows_containing_galaxies)
         .collect_vec();
     let columns_containing_galaxies = (min_y..max_y).into_iter().collect();
-    let mut columns_to_expand = columns_with_galaxies
+    let columns_to_expand = columns_with_galaxies
         .symmetric_difference(&columns_containing_galaxies)
         .collect_vec();
-
-    // dbg!(&rows_to_expand, &columns_to_expand);
-    rows_to_expand.sort();
-    columns_to_expand.sort();
 
     let expanded_galaxies = galaxies
         .into_iter()
         .map(|pos| {
-            let dx = rows_to_expand.partition_point(|i| i < &&pos.x);
-            let dy = columns_to_expand.partition_point(|i| i < &&pos.y);
-            pos + IVec2::new(dx as i32, dy as i32)
+            let dx = rows_to_expand.partition_point(|i| i < &&pos.x) as i64;
+            let dy = columns_to_expand.partition_point(|i| i < &&pos.y) as i64;
+            pos + I64Vec2::new(dx * (ratio - 1), dy * (ratio - 1))
         })
         .collect_vec();
 
@@ -51,12 +49,15 @@ fn process(input: &str) -> String {
                 diff.x + diff.y
             })
         })
-        .sum::<i32>()
+        .sum::<i64>()
         .to_string()
 }
 
 fn main() {
-    println!("Output: {}", process(include_str!("./input.txt")));
+    println!(
+        "Output: {}",
+        process(include_str!("./input.txt"), EXPANSION_RATIO)
+    );
 }
 
 #[cfg(test)]
@@ -73,10 +74,19 @@ mod test {
 ..........
 .......#..
 #...#.....";
-    const ANSWER: &str = "374";
+    const ANSWER: &str = "1030";
+    const EXPANSION_RATIO: i64 = 10;
 
     #[test]
     fn example() {
-        assert_eq!(ANSWER, process(EXAMPLE))
+        assert_eq!(ANSWER, process(EXAMPLE, EXPANSION_RATIO))
+    }
+
+    const ANSWER1: &str = "8410";
+    const EXPANSION_RATIO1: i64 = 100;
+
+    #[test]
+    fn example1() {
+        assert_eq!(ANSWER1, process(EXAMPLE, EXPANSION_RATIO1))
     }
 }
