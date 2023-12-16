3af3130d397bd1cd1fb1daef08cab840cfb337e9
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-16 02:06:54 +0100

    part2 SLOW not solved
    Examples finish quickely but real input takes hours, aleast before not finishing

--- day-12/src/bin/part2.rs
+++ day-12/src/bin/part2.rs
@@ -1,11 +1,13 @@
+use indicatif::ParallelProgressIterator;
 use nom::{
     character::complete::{self, multispace1, one_of},
     multi::{many1, separated_list1},
     sequence::separated_pair,
     IResult, Parser,
 };
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Status {
     Operational,
     Damaged,
@@ -24,84 +26,107 @@ impl TryFrom<char> for Status {
     }
 }
 
-type Mask = u32;
+type Mask = u128;
 
 fn process(input: &str) -> String {
     let (_, springs) = parse(input).unwrap();
 
     springs
-        .into_iter()
-        .map(|(records, span_lengths)| {
+        .into_par_iter()
+        .map(|(mut records, span_lengths)| {
+            let base_records = records.clone();
+            // Make records repeate five times with Status::Unknown inbetween
+            records.push(Status::Unknown);
+            records.extend_from_slice(&base_records);
+            records.push(Status::Unknown);
+            records.extend_from_slice(&base_records);
+            records.push(Status::Unknown);
+            records.extend_from_slice(&base_records);
+            records.push(Status::Unknown);
+            records.extend_from_slice(&base_records);
+
+            let span_lengths: Vec<_> = span_lengths
+                .iter()
+                .cloned()
+                .cycle()
+                .take(span_lengths.len() * 5)
+                .collect();
+
+            // dbg!(records.len(), span_lengths.len());
+
             let operational_mask: Mask = records
                 .iter()
                 .enumerate()
                 .filter_map(|(i, status)| matches!(status, Status::Operational).then_some(i))
-                .fold(0u32, |mask, i| mask | 1 << i);
+                .fold(0, |mask, i| mask | 1 << i);
             let damaged_mask: Mask = records
                 .iter()
                 .enumerate()
                 .filter_map(|(i, status)| matches!(status, Status::Damaged).then_some(i))
-                .fold(0u32, |mask, i| mask | 1 << i);
-            println!("operational: {:032b}", operational_mask);
-            println!("damaged: {:032b}", damaged_mask);
+                .fold(0, |mask, i| mask | 1 << i);
+            // println!("operational: {:032b}", operational_mask);
+            // println!("damaged: {:032b}", damaged_mask);
 
             let spaces = (records.len() as u32) - span_lengths.iter().sum::<u32>();
-            dbg!(spaces);
+            // dbg!(spaces);
 
-            fn find_arrangements(
+            fn count_arrangements(
                 mask: Mask,
                 offset: u32,
                 free_spaces: u32,
                 spans: &[u32],
-                results: &mut Vec<Mask>,
-            ) {
+                operational_mask: Mask,
+                damaged_mask: Mask,
+            ) -> u32 {
                 let Some(span) = spans.first() else {
-                results.push(mask);
-                return;
-            };
+                    return 0;
+                };
+
                 let spans = &spans[1..];
-                let span_mask = u32::MAX >> (32 - span);
+                let span_mask = Mask::MAX >> (Mask::BITS - span);
 
                 if spans.is_empty() {
-                    for used_spaces in 0..=free_spaces {
-                        let new_offset = offset + used_spaces;
-                        find_arrangements(
-                            mask | span_mask << new_offset,
-                            new_offset + span + 1,
-                            free_spaces - used_spaces,
-                            spans,
-                            results,
-                        );
-                    }
+                    (0..=free_spaces)
+                        .filter(|used_spaces| {
+                            let new_offset = offset + used_spaces;
+                            let new_mask = mask | span_mask << new_offset;
+                            new_mask & damaged_mask == damaged_mask
+                                && !new_mask & operational_mask == operational_mask
+                        })
+                        .count() as u32
                 } else {
-                    for used_spaces in 0..free_spaces {
-                        let new_offset = offset + used_spaces;
-                        find_arrangements(
-                            mask | span_mask << new_offset,
-                            new_offset + span + 1,
-                            free_spaces - used_spaces - 1,
-                            spans,
-                            results,
-                        );
-                    }
+                    (0..free_spaces)
+                        .map(|used_spaces| {
+                            let new_offset = offset + used_spaces;
+                            let new_mask = mask | span_mask << new_offset;
+                            if new_mask & operational_mask != 0
+                                || (new_offset > 0
+                                    && !new_mask
+                                        & (damaged_mask & (Mask::MAX >> (Mask::BITS - new_offset)))
+                                        != 0)
+                            {
+                                return 0;
+                            }
+
+                            count_arrangements(
+                                new_mask,
+                                new_offset + span + 1,
+                                free_spaces - used_spaces - 1,
+                                spans,
+                                operational_mask,
+                                damaged_mask,
+                            )
+                        })
+                        .sum()
                 }
             }
 
-            let mut results = Vec::new();
             let spans = span_lengths.as_slice();
-            find_arrangements(0, 0, spaces, spans, &mut results);
-
-            results
-                .into_iter()
-                .filter(|mask| {
-                    mask & damaged_mask == damaged_mask
-                        && !mask & operational_mask == operational_mask
-                })
-                .inspect(|mask| println!("PRODUCED: {:32b}", mask))
-                .count()
+            count_arrangements(0, 0, spaces, spans, operational_mask, damaged_mask)
         })
-        .inspect(|n| println!("NUMBER {n}"))
-        .sum::<usize>()
+        .progress()
+        // .inspect(|n| println!("NUMBER {n}"))
+        .sum::<u32>()
         .to_string()
 }
 
@@ -131,7 +156,7 @@ mod test {
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1";
-    const ANSWER: &str = "21";
+    const ANSWER: &str = "525152";
 
     #[test]
     fn example() {
