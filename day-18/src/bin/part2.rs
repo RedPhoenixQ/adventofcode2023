0828235cbd168ca35831262228ab3ba8cf55b0d9
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-23 02:00:34 +0100

    WIP bad hueristic for finding area

--- day-18/src/bin/part2.rs
+++ day-18/src/bin/part2.rs
@@ -1,10 +1,8 @@
-use std::collections::HashSet;
-
 use itertools::Itertools;
 use nom::{
-    character::complete::{self, hex_digit1, multispace1, one_of, space1},
+    character::complete::{self, alpha1, hex_digit1, multispace1, space1},
     multi::separated_list1,
-    sequence::tuple,
+    sequence::{delimited, tuple},
     IResult, Parser,
 };
 
@@ -21,10 +19,10 @@ impl TryFrom<char> for Direction {
 
     fn try_from(value: char) -> Result<Self, Self::Error> {
         Ok(match value {
-            'U' | 'u' => Direction::Up,
-            'D' | 'd' => Direction::Down,
-            'L' | 'l' => Direction::Left,
-            'R' | 'r' => Direction::Right,
+            'U' | 'u' | '3' => Direction::Up,
+            'D' | 'd' | '1' => Direction::Down,
+            'L' | 'l' | '2' => Direction::Left,
+            'R' | 'r' | '0' => Direction::Right,
             _ => return Err(()),
         })
     }
@@ -46,8 +44,7 @@ impl std::ops::Not for Direction {
 #[derive(Debug)]
 struct Instruction {
     direction: Direction,
-    steps: u8,
-    color: (u8, u8, u8),
+    steps: isize,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -58,111 +55,63 @@ struct Coord {
 
 fn process(input: &str) -> String {
     let (_, instructions) = parse(input).unwrap();
-    dbg!(&instructions);
+    dbg!(&instructions, instructions.len());
 
-    let mut trench: HashSet<Coord> = HashSet::new();
-    let mut pos = Coord { x: 0, y: 0 };
-    let mut left_turns = 0;
+    let mut area = 0;
 
-    for instruction in instructions {
-        for _ in 0..instruction.steps {
-            trench.insert(pos);
-            match instruction.direction {
-                Direction::Down => pos.y += 1,
-                Direction::Up => pos.y -= 1,
-                Direction::Left => pos.x -= 1,
-                Direction::Right => pos.x += 1,
-            }
-        }
+    let mut inst_iter = instructions.chunks(2);
+
+    while let Some([inst1, inst2]) = inst_iter.next() {
+        let w = match inst1.direction {
+            Direction::Down | Direction::Right => inst1.steps,
+            Direction::Up | Direction::Left => -inst1.steps,
+        };
+        let h = match inst2.direction {
+            Direction::Down | Direction::Right => inst2.steps,
+            Direction::Up | Direction::Left => -inst2.steps,
+        };
+        dbg!(inst1, inst2, w, h, w * h);
+        area += w * h;
     }
 
-    dbg!(&trench, trench.len());
+    // dbg!(&trench, trench.len());
 
-    let min_x = trench.iter().map(|coord| coord.x).min().unwrap();
-    let max_x = trench.iter().map(|coord| coord.x).max().unwrap();
-    let min_y = trench.iter().map(|coord| coord.y).min().unwrap();
-    let max_y = trench.iter().map(|coord| coord.y).max().unwrap();
+    // for y in min_y..=max_y {
+    //     for x in min_x..=max_x {
+    //         print!(
+    //             "{}",
+    //             trench
+    //                 .contains(&Coord { x, y })
+    //                 .then_some('#')
+    //                 .unwrap_or('.')
+    //         );
+    //     }
+    //     println!("");
+    // }
 
-    dbg!(min_x, max_x, min_y, max_y);
-
-    let mut todo: Vec<(Coord, Direction)> = Vec::from(&[(Coord { x: 1, y: 1 }, Direction::Right)]);
-
-    while let Some((coord, from)) = todo.pop() {
-        dbg!(todo.len());
-
-        trench.insert(coord);
-
-        for direction in [
-            Direction::Down,
-            Direction::Up,
-            Direction::Left,
-            Direction::Right,
-        ] {
-            if !from == direction {
-                continue;
-            }
-            let next = match direction {
-                Direction::Down => Coord {
-                    x: coord.x,
-                    y: coord.y + 1,
-                },
-                Direction::Up => Coord {
-                    x: coord.x,
-                    y: coord.y - 1,
-                },
-                Direction::Left => Coord {
-                    x: coord.x - 1,
-                    y: coord.y,
-                },
-                Direction::Right => Coord {
-                    x: coord.x + 1,
-                    y: coord.y,
-                },
-            };
-            if trench.contains(&next) {
-                continue;
-            }
-            todo.push((next, direction));
-        }
-    }
-
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            print!(
-                "{}",
-                trench
-                    .contains(&Coord { x, y })
-                    .then_some('#')
-                    .unwrap_or('.')
-            );
-        }
-        println!("");
-    }
-
-    trench.len().to_string()
+    (area - 1).to_string()
 }
 
 fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
     separated_list1(
         multispace1,
-        tuple((
-            one_of("UDLR").map(|c| c.try_into().unwrap()),
-            space1,
-            complete::u8,
-            tuple((space1, complete::char('('), complete::char('#'))),
-            hex_digit1.map(|colorcode: &str| {
-                (0..6)
-                    .step_by(2)
-                    .map(|i| u8::from_str_radix(&colorcode[i..i + 2], 16).unwrap())
-                    .collect_tuple()
-                    .unwrap()
-            }),
+        delimited(
+            tuple((alpha1, space1)),
+            tuple((
+                complete::u8,
+                tuple((space1, complete::char('('), complete::char('#'))),
+                hex_digit1.map(|colorcode: &str| {
+                    let steps =
+                        isize::from_str_radix(&colorcode[0..colorcode.len() - 1], 16).unwrap();
+                    let direction = colorcode.chars().last().unwrap().try_into().unwrap();
+                    Instruction { direction, steps }
+                }),
+            )),
             complete::char(')'),
-        ))
-        .map(|(direction, _, steps, _, color, _)| Instruction {
-            direction,
-            steps,
-            color,
+        )
+        .map(|(d, _, inst)| Instruction {
+            direction: inst.direction,
+            steps: d as isize,
         }),
     )
     .parse(input)
@@ -190,7 +139,7 @@ mod test {
     U 3 (#a77fa3)
     L 2 (#015232)
     U 2 (#7a21e3)";
-    const ANSWER: &str = "62";
+    const ANSWER: &str = "952408144115";
 
     #[test]
     fn example() {
