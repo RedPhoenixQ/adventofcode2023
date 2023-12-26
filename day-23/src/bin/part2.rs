da2b792b5303631d9e26549534e3ea650a5b8fb0
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-26 02:27:58 +0100

    part2 solved

--- day-23/src/bin/part2.rs
+++ day-23/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 use petgraph::prelude::*;
 
@@ -49,17 +49,17 @@ fn process(input: &str) -> String {
         x: (input.lines().next().unwrap().len() - 2) as isize,
     };
 
-    let mut graph: DiGraph<Coord, ()> = DiGraph::new();
+    let mut graph: UnGraph<Coord, usize> = UnGraph::new_undirected();
 
     let start_coord = Coord { x: 1, y: 0 };
     let start_index = graph.add_node(start_coord);
-    let mut coord_node: HashMap<Coord, NodeIndex> = HashMap::from([(start_coord, start_index)]);
+    let mut junction_node: HashMap<Coord, NodeIndex> = HashMap::from([(start_coord, start_index)]);
 
-    let mut steps = VecDeque::from([((start_coord, start_coord), start_index)]);
+    let mut walker = VecDeque::from([((start_coord, start_coord), start_index, 1)]);
 
-    dbg!(&grid, &graph, &steps);
+    // dbg!(&grid, &graph, &walker);
 
-    while let Some(((coord, prev), prev_i)) = steps.pop_front() {
+    while let Some(((coord, prev), prev_i, steps)) = walker.pop_front() {
         for diff in Coord::CARDINAL_DIRECTIONS {
             let next = coord + diff;
             if next == prev {
@@ -68,59 +68,69 @@ fn process(input: &str) -> String {
             let Some(c) = grid.get(&next) else {
                 continue;
             };
-            match (c, diff) {
-                ('#', _)
-                | ('^', Coord { x: 0, y: 1 })
-                | ('>', Coord { x: -1, y: 0 })
-                | ('v', Coord { x: 0, y: -1 })
-                | ('<', Coord { x: 1, y: 0 }) => continue,
-                _ => {}
+            if c == &'#' {
+                continue;
             }
-            let node_i = coord_node
-                .get(&next)
-                .and_then(|n| Some(*n))
-                .unwrap_or_else(|| {
-                    let n = graph.add_node(next);
-                    coord_node.insert(next, n);
-                    n
-                });
-            graph.update_edge(prev_i, node_i, ());
-            steps.push_back(((next, coord), node_i));
+
+            let is_junciton = Coord::CARDINAL_DIRECTIONS
+                .into_iter()
+                .filter(|&d| {
+                    let peek = next + d;
+                    if peek == coord {
+                        return false;
+                    }
+                    grid.get(&peek).is_some_and(|c| !matches!(c, '.' | '#'))
+                })
+                .count()
+                > 1;
+
+            if is_junciton || next == end_coord {
+                let node_i = junction_node
+                    .get(&next)
+                    .and_then(|n| Some(*n))
+                    .unwrap_or_else(|| {
+                        let n = graph.add_node(next);
+                        walker.push_back(((next, coord), n, 1));
+                        junction_node.insert(next, n);
+                        n
+                    });
+                graph.update_edge(prev_i, node_i, steps);
+            } else {
+                walker.push_back(((next, coord), prev_i, steps + 1));
+            }
         }
     }
-    // println!("{:?}", petgraph::dot::Dot::new(&graph));
-    let end_index = coord_node
+    //println!("{:?}", petgraph::dot::Dot::new(&graph));
+    let end_index = junction_node
         .get(&end_coord)
         .expect("end node to exist in graph");
 
-    #[derive(Debug, PartialEq, Eq, PartialOrd)]
-    struct QueueItem {
-        steps: usize,
-        node: NodeIndex,
-    }
-    impl Ord for QueueItem {
-        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-            self.steps.cmp(&other.steps)
-        }
-    }
-
-    let mut queue: BinaryHeap<QueueItem> = BinaryHeap::from([QueueItem {
-        steps: 0,
-        node: start_index,
-    }]);
-
+    let mut queue = Vec::from([(start_index, vec![start_index], 0)]);
     let mut max_steps = 0;
 
-    while let Some(QueueItem { steps, node }) = queue.pop() {
+    while let Some((node, prev, steps)) = queue.pop() {
+        //dbg!(&queue.len(), &steps, &node, &prev);
         if &node == end_index {
+            // dbg!(&steps, &prev);
+            // prev.into_iter()
+            //     .for_each(|idx| println!("{:?}", graph.node_weight(idx)));
             max_steps = max_steps.max(steps);
             continue;
         }
         for neighbor in graph.neighbors(node) {
-            queue.push(QueueItem {
-                steps: steps + 1,
-                node: neighbor,
-            });
+            if prev.contains(&neighbor) {
+                continue;
+            }
+            let mut new_prev = prev.clone();
+            new_prev.push(neighbor);
+            queue.push((
+                neighbor,
+                new_prev,
+                steps
+                    + graph
+                        .edge_weight(graph.find_edge(node, neighbor).unwrap())
+                        .unwrap(),
+            ));
         }
     }
 
@@ -158,7 +168,7 @@ mod test {
 #.###.###.#.###.#.#v###
 #.....###...###...#...#
 #####################.#";
-    const ANSWER: &str = "94";
+    const ANSWER: &str = "154";
 
     #[test]
     fn example() {
