4cf381ed527acf094e28e3ee7b6a2b85e4848447
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-26 01:28:30 +0100

    Store linear path length in edge weights

--- day-23/src/bin/part1.rs
+++ day-23/src/bin/part1.rs
@@ -49,17 +49,17 @@ fn process(input: &str) -> String {
         x: (input.lines().next().unwrap().len() - 2) as isize,
     };
 
-    let mut graph: DiGraph<Coord, ()> = DiGraph::new();
+    let mut graph: DiGraph<Coord, usize> = DiGraph::new();
 
     let start_coord = Coord { x: 1, y: 0 };
     let start_index = graph.add_node(start_coord);
-    let mut coord_node: HashMap<Coord, NodeIndex> = HashMap::from([(start_coord, start_index)]);
+    let mut junction_node: HashMap<Coord, NodeIndex> = HashMap::from([(start_coord, start_index)]);
 
-    let mut steps = VecDeque::from([((start_coord, start_coord), start_index)]);
+    let mut walker = VecDeque::from([((start_coord, start_coord), start_index, 1)]);
 
-    dbg!(&grid, &graph, &steps);
+    dbg!(&grid, &graph, &walker);
 
-    while let Some(((coord, prev), prev_i)) = steps.pop_front() {
+    while let Some(((coord, prev), prev_i, steps)) = walker.pop_front() {
         for diff in Coord::CARDINAL_DIRECTIONS {
             let next = coord + diff;
             if next == prev {
@@ -76,20 +76,37 @@ fn process(input: &str) -> String {
                 | ('<', Coord { x: 1, y: 0 }) => continue,
                 _ => {}
             }
-            let node_i = coord_node
-                .get(&next)
-                .and_then(|n| Some(*n))
-                .unwrap_or_else(|| {
-                    let n = graph.add_node(next);
-                    coord_node.insert(next, n);
-                    n
-                });
-            graph.update_edge(prev_i, node_i, ());
-            steps.push_back(((next, coord), node_i));
+
+            let is_junciton = Coord::CARDINAL_DIRECTIONS
+                .into_iter()
+                .filter(|&d| {
+                    let peek = next + d;
+                    if peek == coord {
+                        return false;
+                    }
+                    grid.get(&peek).is_some_and(|c| !matches!(c, '.' | '#'))
+                })
+                .count()
+                > 1;
+
+            if is_junciton || next == end_coord {
+                let node_i = junction_node
+                    .get(&next)
+                    .and_then(|n| Some(*n))
+                    .unwrap_or_else(|| {
+                        let n = graph.add_node(next);
+                        walker.push_back(((next, coord), n, 1));
+                        junction_node.insert(next, n);
+                        n
+                    });
+                graph.update_edge(prev_i, node_i, steps);
+            } else {
+                walker.push_back(((next, coord), prev_i, steps + 1));
+            }
         }
     }
     // println!("{:?}", petgraph::dot::Dot::new(&graph));
-    let end_index = coord_node
+    let end_index = junction_node
         .get(&end_coord)
         .expect("end node to exist in graph");
 
@@ -118,7 +135,10 @@ fn process(input: &str) -> String {
         }
         for neighbor in graph.neighbors(node) {
             queue.push(QueueItem {
-                steps: steps + 1,
+                steps: steps
+                    + graph
+                        .edge_weight(graph.find_edge(node, neighbor).unwrap())
+                        .unwrap(),
                 node: neighbor,
             });
         }
